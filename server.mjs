e8a5b4556788c683743bb6005bb06ccdda68bd7b
Andrew Klenner <atklenner@gmail.com>
2023-02-09 11:10:17 -0800

    solved weird express errors I've never seen before?

--- server.mjs
+++ server.mjs
@@ -1,25 +1,28 @@
-const express = require("express")
+import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
+import { nanoid } from "nanoid";
 
 const db = {
   "abcd": "https://expressjs.com/"
 };
 
 app.use(express.static("public"));
-app.use(express.urlencoded("extended"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (_req, res) => {
   res.send("./form.html");
 })
 
 app.post("/extend", (req, res) => {
-  console.log(req.body);
-  res.redirect("/");
+  let id = nanoid(10);
+  db[id] = req.body.url;
+  console.log(db[id]);
+  res.json(id);
 })
 
-app.get("/:stuff", (req, res) => {
-  const url = db[req.params.stuff];
+app.get("*", (req, res) => {
+  const url = db[req.query.id];
   res.redirect(url);
 })
 
