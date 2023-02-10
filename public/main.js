c8f8e8c61ee7b6c62e518e31ba6c53525122fcdb
Andrew Klenner <atklenner@gmail.com>
2023-02-10 10:48:19 -0800

    user can now adjust link without getting a new id

---
+++ public/main.js
@@ -0,0 +1,27 @@
+const form = document.querySelector("form");
+const urlInput = document.querySelector(".url-input");
+const text = document.querySelector("textarea");
+const link = document.querySelector("a");
+const code = document.querySelector("code");
+let id;
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  if (!id) {
+    let res = await fetch("/extend", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ url: urlInput.value })
+    })
+    id = await res.json();
+  }
+  const extendedURL = `localhost:3000/${filterText(text.value)}/?id=${id}`;
+  link.textContent = extendedURL;
+  link.href = extendedURL;
+  code.textContent = extendedURL;
+})
+
+function filterText(str) {
+  return str.replace(/[{}|\\\^\[\]`;\/\?:@&=+$\,.]/g, "").replaceAll(" ", "-");
+}
